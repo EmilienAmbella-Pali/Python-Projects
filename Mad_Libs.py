3067941c534a85db7121dd2c8fa2884c0b019d4e
Emilien Ambella-Pali <141251372+EmilienAmbella-Pali@users.noreply.github.com>
2023-08-08 11:46:00 +0100

    Add files via upload

---
+++ Mad_Libs.py
@@ -0,0 +1,23 @@
+food = input("Enter a type of food: ")
+2
+jamie = input("Enter a girl's name: ")
+3
+adj1 = input("Enter an adjective: ")
+4
+bird = input("Enter a noun: ")
+5
+verb1 = input("Enter a verb in the past tense: ")
+6
+verb2 = input("Enter another verb in the past tense: ")
+7
+verb3 = input("Enter a third verb in the past tense: ")
+8
+noun1 = input("Enter a noun: ")
+9
+  
+10
+story = "It was " + food + " day at school, and " + jamie + " was super " + adj1 + " for lunch. But when she went outside to eat, a " + bird + " stole her " + food + "! " + jamie + " chased the " + bird + " all over school. She " + verb1 + ", " + verb2 + ", and " + verb3 + " through the playground. Then she tripped on her " + noun1 + " and the " + bird + " escaped! Luckily, " + jamie + "'s friends were willing to share their " + food + " with her."
+11
+  
+12
+print(story)
